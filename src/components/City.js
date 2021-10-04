2c151adc9fb0c6ec5b0dad2607518729d43a9133
ABY <abyener54@gmail.com>
2021-10-04 22:59:14 +0300

    Add city filter

--- src/components/City.js
+++ src/components/City.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default function City() {
 
     const [cities,setCities] = useState([]);
+    const [citiesTampon,setCitiesTampon] = useState(null);
 
     useEffect(()=>{
         getCities()
@@ -22,18 +23,51 @@ export default function City() {
                 setCities(res.data);
             })
     }
+
+    const filterCities = (e) => {
+        var input= e.target.value;
+        console.log(input);
+        
+        setCitiesTampon((prevCity)=> {
+            return cities.filter(city=>{
+                return city.toLowerCase().includes(input.toLowerCase())
+            })
+        })
+    }
     return (
-        <div className="row">
-            {cities.map(city=>(
-                <div className="col l3 m4 s6" key={city}>
-                    <div className="card blue-grey darken-1">
-                        <div className="card-content white-text">
-                            <p>{city}</p>
-                        </div>
+        <div>
+            <div className="row container">
+                <form className="col s12 l12 m12">
+                    <div className="input-field col s12">
+                        <input id="city" className="input-field" type="text"  onChange={filterCities}/>
+                        <label htmlFor="city" className="deep-orange-text">Search City</label>
                     </div>
-                </div>
-            ))}
-            
+                </form>
+            </div>
+            <div className="row">
+                {
+                    citiesTampon!=null ?(
+                        citiesTampon.map(city=>(
+                            <div className="col l3 m4 s6" key={city}>
+                                <div className="card blue-grey darken-1">
+                                    <div className="card-content white-text">
+                                        <p>{city}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    ) :
+                    (cities.map(city=>(
+                        <div className="col l3 m4 s6" key={city}>
+                            <div className="card blue-grey darken-1">
+                                <div className="card-content white-text">
+                                    <p>{city}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )))
+                }
+            </div>
         </div>
     )
 }
